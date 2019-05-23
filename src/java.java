0f30d1d25a831653162fec6a73503f3b643afb7e
vemanisha <manishavermaa786@gmail.com>
2019-05-23 13:42:06 -0400

    123456 y1

---
+++ src/java.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manis
+ */
+public class java {
+    private int hl;
+
+    /**
+     * @return the hl
+     */
+    public int getHl() {
+        return hl;
+    }
+
+    /**
+     * @param hl the hl to set
+     */
+    public void setHl(int hl) {
+        this.hl = hl;
+    }
+}
